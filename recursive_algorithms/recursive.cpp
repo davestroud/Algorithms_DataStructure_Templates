ebc0060d19a45709f0e73d69c606489956f100f0
davestroud <david@davidstroud.me>
2016-08-16 16:43:27 -0500

    add notes giving examples of recursion

--- recursive_algorithms/recursive.cpp
+++ recursive_algorithms/recursive.cpp
@@ -14,3 +14,20 @@ int main( )
     cout << "f(5) = " << f( 5 ) << endl;
     return 0;
 }
+
+
+/*
+A function that is defined in terms of itself is called recursive.
+The Fibonacci sequence is a classic example of recursion:
+
+Need at least one base case; as well as a recursive call
+
+Factorials:
+1! = 1			       =1				     =1
+2! = 2*1		       =2*1!				=2
+3! = 3*2*1	   	   =3*2!				=6
+4! = 4*3*2*1		   =4*3!				=24
+5! = 5*4*3*2*1		 =5*4!				=120
+
+ 	                n! = n * (n-1)!
+*/
