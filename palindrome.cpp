2e650d0666ce16fc3f361f5cf1e4ab33e437fa5a
davestroud <david@davidstroud.me>
2016-08-25 08:51:21 -0500

    add iterative algorithm for palidrome

--- palindrome.cpp
+++ palindrome.cpp
@@ -38,6 +38,10 @@ bool validPalindrome(string s)
     // handout, implement the details of the recursive
     // isPalindrome function.
 
-    return false;
+    int n = s.length();
+    for (int i = 0; i < n / 2; i++) {
+      if (s[i] != s[n - i -1]) return false;
+    }
+
+    return true;
 }
-  
