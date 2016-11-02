1a69ef13f0b11d9a9916cccecc3049367505b32b
davestroud <david@davidstroud.me>
2016-11-02 14:04:18 -0500

    add linked list folder

--- Linked_Lists/NumberList.h
+++ Linked_Lists/NumberList.h
@@ -1,18 +1,16 @@
-// Specification file fo the NumberList class
+// Specification file for the NumberList class
 #ifndef NUMBERLIST_H
 #define NUMBERLIST_H
 
-class NumberList
-{
+class NumberList {
 private:
   // Declare a structure for the list
-  struct ListNode
-  {
-    double value;                // The value in this node
-    struct ListNode *next;       // To point to the next node
+  struct ListNode {
+    double value;                 // The value in this node
+    struct ListNode *next;        // To point to the next node
   };
 
-  ListNode *head;                 // List head pointer
+  ListNode *head;                 //List head pointer
 
 public:
   // Constructor
