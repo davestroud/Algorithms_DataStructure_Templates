384a0fcfe893a50fdfbe76788393c50f0bd5edc2
davestroud <david@davidstroud.me>
2016-11-09 10:10:21 -0600

    add push_back

--- Cave_of_Programming/twoDimensionalVectors.cpp
+++ Cave_of_Programming/twoDimensionalVectors.cpp
@@ -6,6 +6,10 @@ int main(int argc, char const *argv[]) {
 
     vector<vector<int> > grid(5, vector<int>(5, 2));
 
+    grid[0].push_back(6);
+    grid[1].push_back(8);
+    grid[2].push_back(4);
+
     for(int row = 0; row < grid.size(); row++) {
       for(int col = 0; col < grid[row].size(); col++) {
         cout << grid[row][col] << flush;
