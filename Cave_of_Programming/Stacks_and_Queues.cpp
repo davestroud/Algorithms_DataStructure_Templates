37b1d3f5b8db942a6f89893481d43fdf6e7876ff
davestroud <david@davidstroud.me>
2016-11-09 15:39:11 -0600

    add Stack and Queue algos

--- Cave_of_Programming/Stacks_and_Queues.cpp
+++ Cave_of_Programming/Stacks_and_Queues.cpp
@@ -5,17 +5,17 @@ using namespace std;
 
 class Test {
 private:
-    string name;
+  string name;
 
 public:
-    Test(string name):
+  Test(string name):
             name(name) {
 
 
             }
 
             ~Test() {
-                // cout << "Object desroyed" << endl;
+                //cout << "Object destroyed" << endl;
             }
 
             void print() {
@@ -25,6 +25,65 @@ public:
 
 
 int main(int argc, char const *argv[]) {
-  /* code */
+
+  // LIFO
+  stack<Test> testStack;
+
+  testStack.push(Test("David"));
+  testStack.push(Test("Jill"));
+  testStack.push(Test("Jennifer"));
+
+  cout << endl;
+
+  /*
+  * This is invalid code! Object is destroyed
+  Test &test1 = testStack.top();
+  testStack.pop();
+  test1.print();
+  testStack.pop();  // Reference refers to destroyed objects
+  */
+
+  while(testStack.size() > 0) {
+    Test &test = testStack.top();
+    test.print();
+    testStack.pop();
+  }
+
+  // Break between algorithms.
+  cout << endl;
+
+  // FIFO
+  queue<Test> testQueue;
+
+  testQueue.push(Test("David"));
+  testQueue.push(Test("Jill"));
+  testQueue.push(Test("Jennifer"));
+
+  cout << endl;
+
+  /*
+  * This is invalid code! Object is destroyed
+  Test &test1 = testStack.top();
+  testStack.pop();
+  test1.print();
+  testStack.pop();  // Reference refers to destroyed objects
+  */
+
+  testQueue.back().print(); // Last person to enter queue.
+
+  cout << endl; 
+
+  while(testQueue.size() > 0) {
+    Test &test = testQueue.front();
+    test.print();
+    testQueue.pop();
+  }
+
+
+
+
+
+
+
   return 0;
 }
